d47543ea943620dcd36f7e1c7f0b511b84275e12
guysela567 <guysela567@gmail.com>
2020-07-22 18:41:49 +0300

    fixed sound issues, added number colors and comments

---
+++ colors.js
@@ -0,0 +1,13 @@
+const revealedColor = 189;
+const strokeColor = 123;
+
+const colors = [
+  '#0100fe',
+  '#017f01',
+  '#fe0000',
+  '#010080',
+  '#810102',
+  '#008081',
+  '#000000',
+  '#808080'
+]
--- sketch.js
+++ sketch.js
@@ -3,10 +3,7 @@ const rows = 20;
 const size = 40;
 const totalBombs = 50;
 const gridOffsetY = 100;
-
-// colors
-const revealedColor = 189;
-const strokeColor = 123;
+let gameIsOver = false;
 
 /*
 
@@ -30,8 +27,8 @@ let sevenSegmentFont;
 // set timer
 let time = 0;
 
-setInterval(() => {
-	time++;
+const timeInterval = setInterval(() => {
+  time++;
 }, 1000)
 
 // sounds
@@ -43,124 +40,139 @@ let gameOverSound;
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 function preload() {
-	blockImg = loadImage('assets/unrevealed_block.png');
-	sevenSegmentFont = loadFont('assets/Seven_Segment_Bold.ttf');
-	revealSound = loadSound('assets/shatter.mp3');
-	gameOverSound = loadSound('assets/explosion.mp3');
+  blockImg = loadImage('assets/unrevealed_block.png');
+  sevenSegmentFont = loadFont('assets/Seven_Segment_Bold.ttf');
+  defaultFont = loadFont('assets/Default_Font.ttf');
+  revealSound = loadSound('assets/shatter.mp3');
+  gameOverSound = loadSound('assets/explosion.mp3');
 }
 
 function setup() {
-	createCanvas(cols * size, rows * size + gridOffsetY);
-	textSize(size / 1.4);
-	textAlign(CENTER, CENTER);
+  createCanvas(cols * size, rows * size + gridOffsetY).center('horizontal');
+  textSize(size / 1.4);
+  textAlign(CENTER, CENTER);
 
-	arrangeGrid();
+  arrangeGrid();
 
-	revealSound.setVolume(0.4);
+  revealSound.setVolume(0.4);
+
+  // set pickaxe cursor
+  cursor('assets/Pickaxe_Cursor.cur');
 }
 
 function draw() {
-	background(revealedColor);
+  background(revealedColor);
 
-	// menu
-	push();
+  // menu
+  push();
 
-	fill(255, 0, 0);
-	noStroke();
-	textSize(40);
-	textFont(sevenSegmentFont);
-	textAlign(LEFT, CENTER);
+  fill(255, 0, 0);
+  noStroke();
+  textSize(40);
+  textFont(sevenSegmentFont);
+  textAlign(LEFT, CENTER);
 
-	// show timer
-	fill(50, 0, 0, 100);
-	text('888', 50, 50);
-	fill(220, 0, 0);
-	text(nf(time, 3), 50, 50);
+  // show timer
+  fill(50, 0, 0, 100);
+  text('888', 50, 50);
+  fill(220, 0, 0);
+  text(nf(time, 3), 50, 50);
 
-	// show flags left
-	fill(50, 0, 0, 100);
-	text('888', width - 120, 50);
-	fill(220, 0, 0);
-	text(nf(flagsLeft(), 3), width - 120, 50);
+  // show flags left
+  fill(50, 0, 0, 100);
+  text('888', width - 120, 50);
+  fill(220, 0, 0);
+  text(nf(flagsLeft(), 3), width - 120, 50);
 
-	pop();
+  pop();
 
-	showGrid();
+  showGrid();
 }
 
 function createGrid() {
-	grid = Array(cols);
-	for (let i = 0; i < cols; i++) {
-		grid[i] = Array(rows);
-	}
+  grid = Array(cols);
+  for (let i = 0; i < cols; i++) {
+    grid[i] = Array(rows);
+  }
 
-	// add cells
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			grid[i][j] = new Cell(i, j);
-		}
-	}
+  // add cells
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Cell(i, j);
+    }
+  }
 }
 
 function showGrid() {
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			grid[i][j].show();
-		}
-	}
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].show();
+    }
+  }
 }
 
 function addBombs() {
-	const options = [];
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			options.push({x: i, y: j});
-		}
-	}
+  const options = [];
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      options.push({
+        x: i,
+        y: j
+      });
+    }
+  }
 
-	for (let k = 0; k < totalBombs; k++) {
-		const index = floor(random(options.length));
-		const choice = options[index];	
-		grid[choice.x][choice.y].isBomb = true;
-		options.splice(index, 1);
-	}
+  for (let k = 0; k < totalBombs; k++) {
+    const index = floor(random(options.length));
+    const choice = options[index];
+    grid[choice.x][choice.y].isBomb = true;
+    options.splice(index, 1);
+  }
 }
 
 
 function arrangeGrid() {
-	createGrid();
-	addBombs();
+  createGrid();
+  addBombs();
 
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			grid[i][j].countNeighbors();
-		}
-	}
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].countNeighbors();
+    }
+  }
 }
 
 function mousePressed() {
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			if (grid[i][j].mouseHover()) {
-				if (mouseButton == LEFT && !grid[i][j].isFlagged) {
-					// left click
-					grid[i][j].reveal();
+  if (gameIsOver) return;
 
-					if (grid[i][j].isBomb) gameOver() 
-					else revealSound.play();
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      // check if cursor is on the cell
+      if (grid[i][j].mouseHover() && !grid[i][j].revealed) {
 
-				} else if (mouseButton == RIGHT && !grid[i][j].revealed) {
-					// right click
-					grid[i][j].isFlagged = !grid[i][j].isFlagged;
-				}
-			}
-		}
-	}
+        if (mouseButton == LEFT && !grid[i][j].isFlagged) {
+          // left click
+          grid[i][j].reveal();
+
+          // check if player revealed bomb
+          if (grid[i][j].isBomb) gameOver();
+          else revealSound.play();
+
+        } else if (mouseButton == RIGHT) {
+          // right click
+          grid[i][j].isFlagged = !grid[i][j].isFlagged;
+        }
+      }
+    }
+  }
 }
 
 function gameOver() {
-	gameOverSound.play();
-	noLoop();
+  gameIsOver = true;
+  gameOverSound.play();
+  clearInterval(timeInterval);
+
+  // TODO: reveal all remaining bombs
 }
 
 
@@ -168,8 +180,8 @@ function flagsLeft() {
   let totalFlags = 0;
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-			if (grid[i][j].isFlagged) totalFlags++;
+      if (grid[i][j].isFlagged) totalFlags++;
     }
-	}
-	return totalBombs - totalFlags;
+  }
+  return totalBombs - totalFlags;
 }
